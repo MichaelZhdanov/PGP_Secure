c2c71fbada5eec535bc137c213bd21b05c030cd9
Mikhail Zhdanov <mikhail.zhdanov@ntprog.com>
2025-04-05 03:38:43 +0300

    Menu - 1 and 3 options are OK

--- pyhton_env/menu.py
+++ pyhton_env/menu.py
@@ -3,46 +3,52 @@
 import os
 import sys
 
-choice_readable = ""
-
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def show_menu():
-    clear_screen()
-    print("-----------------------------")
-    print("  M A I N   M E N U")
-    print("-----------------------------")
-    print("1) Option 1: Encrypt file")
-    print("2) Option 2: Decrypt file")
-    print("3) Quit")
-
     while True:
+        clear_screen()
+        print("-----------------------------")
+        print("  M A I N   M E N U")
+        print("-----------------------------")
+        print("1) Option 1: Encrypt file")
+        print("2) Option 2: Decrypt file")
+        print("3) Quit")
+
         try:
-            choice = int(input("Please enter your choice (1 - 3)"))
-            handle_choice(choice)
-            break
+            choice = int(input("Please enter your choice (1 - 3): "))
+            action = handle_choice(choice)
+            
+            if action in ("encryption", "decryption"):
+                return action
+            elif action == "quit":
+                sys.exit(0)
+                
         except ValueError:
             print("Invalid input", file=sys.stderr)
-            continue
+            input("Press Enter to continue...")
 
 def handle_choice(choice):
     if choice == 1:
-        choice_readable == "encryption"
+        return "encryption"
     elif choice == 2:
-        choice_readable == "decryption"
+        return "decryption"
     elif choice == 3:
-        print("Exiting program...")
-        sys.exit(0)
+        print("Exiting...")
+        return "quit"
     else:
-        print("Select 1 -3!", file=sys.stderr)
-        input("Press Enter to countinie...")
-        show_menu()
-        return choice_readable
+        print("Select 1-3!", file=sys.stderr)
+        input("Press Enter to continue...")
+        return None
 
-    input("Press Enter to countinie...")
-    show_menu()
-
+# Check with print
 if __name__ == "__main__":
-    show_menu()
-
+    choice = show_menu()
+    print(f"Selected action: {choice}")
+    
+    # Calling other scripts based on the choice
+    # if choice == "encryption":
+    #     os.system("python ./encryption.py")
+    # elif choice == "decryption":
+    #     os.system("python ./decryption.py")
