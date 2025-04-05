aa2b0f41ab8cc4419b4d6d2857c3d8b780a64522
Mikhail Zhdanov <mikhail.zhdanov@ntprog.com>
2025-04-05 13:13:47 +0300

    create package version

--- cli-gpg.py
+++
@@ -1,8 +0,0 @@
-#!/usr/env python
-
-import os
-def get_current_dir():
-    return str(Path(__file__).resolve().parent)
-
-current_dir = get_current_dir()
-os.system(f"python {current_dir}/python_env/menu.py")
